a40b7aa6507f7230a365f531fb2625cab003f73f
Benjamin Tucker <benjamint@bsquare.com>
2023-01-31 14:45:56 +0000

    Version 0.0.4

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
-	version string = "0.0.3"
+	version string = "0.0.4"
 
 	// goreleaser can also pass the specific commit if you want
 	// commit  string = ""
