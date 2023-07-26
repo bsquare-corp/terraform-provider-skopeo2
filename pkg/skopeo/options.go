d9358ce376e8feea52eebd9c97598b6da48e5675
Benjamin Tucker <benjamint@bsquare.com>
2023-07-26 09:20:51 +0100

    Correct the user agent string

--- pkg/skopeo/options.go
+++ pkg/skopeo/options.go
@@ -6,7 +6,7 @@ import (
 	"github.com/containers/image/v5/types"
 )
 
-var defaultUserAgent = "terraform-skopeo/1.5.1"
+var defaultUserAgent = "terraform-provider-skopeo2/0.0.8"
 
 type GlobalOptions struct {
 	Debug              bool          // Enable debug output
