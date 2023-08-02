355f66cb5b739766a26aff0f80801cd69515bdbd
Benjamin Tucker <benjamint@bsquare.com>
2023-08-02 14:52:56 +0100

    Code format local dev code

--- examples/local-development/main.tf
+++ examples/local-development/main.tf
@@ -14,14 +14,14 @@ terraform {
 resource "skopeo2_copy" "default" {
   count = 1
   source {
-    image          = "docker://753989949864.dkr.ecr.us-west-1.amazonaws.com/ecr-public/docker/library/busybox:uclibc"
-    login_username = "AWS"
+    image                 = "docker://753989949864.dkr.ecr.us-west-1.amazonaws.com/ecr-public/docker/library/busybox:uclibc"
+    login_username        = "AWS"
     login_password_script = "aws --profile bsquare-jenkins2-moved ecr get-login-password --region=us-west-1"
-    timeout = 9
+    timeout               = 9
   }
   destination {
-    image          = "docker://329020582682.dkr.ecr.eu-west-2.amazonaws.com/copytest:latest-${count.index}"
-    login_username = "AWS"
+    image                 = "docker://329020582682.dkr.ecr.eu-west-2.amazonaws.com/copytest:latest-${count.index}"
+    login_username        = "AWS"
     login_password_script = "aws ecr get-login-password --region=eu-west-2"
   }
   preserve_digests = true
