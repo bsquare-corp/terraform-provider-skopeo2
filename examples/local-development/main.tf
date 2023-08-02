eb9e2ba0cd44097bbf4a55a3545fed745c0947d0
Benjamin Tucker <benjamint@bsquare.com>
2023-08-02 14:07:16 +0100

    Local testing using the login_password_script

--- examples/local-development/main.tf
+++ examples/local-development/main.tf
@@ -11,46 +11,23 @@ terraform {
   required_version = ">= 0.15"
 }
 
-provider "aws" {
-  alias  = "source"
-  region = "us-west-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::753989949864:role/qa-cross-account-access"
-  }
-}
-
-provider "aws" {
-  alias  = "dest"
-  region = "eu-west-2"
-
-  assume_role {
-    role_arn = "arn:aws:iam::753989949864:role/qa-cross-account-access"
-  }
-}
-
-data "aws_ecr_authorization_token" "source" {
-  provider = aws.source
-}
-
-data "aws_ecr_authorization_token" "dest" {
-  provider = aws.dest
-}
-
 resource "skopeo2_copy" "default" {
   count = 1
   source {
     image          = "docker://753989949864.dkr.ecr.us-west-1.amazonaws.com/ecr-public/docker/library/busybox:uclibc"
-    login_username = data.aws_ecr_authorization_token.source.user_name
-    login_password = data.aws_ecr_authorization_token.source.password
+    login_username = "AWS"
+    login_password_script = "aws --profile bsquare-jenkins2-moved ecr get-login-password --region=us-west-1"
+    timeout = 9
   }
   destination {
     image          = "docker://329020582682.dkr.ecr.eu-west-2.amazonaws.com/copytest:latest-${count.index}"
-    login_username = data.aws_ecr_authorization_token.dest.user_name
-    login_password = data.aws_ecr_authorization_token.dest.password
+    login_username = "AWS"
+    login_password_script = "aws ecr get-login-password --region=eu-west-2"
   }
-  docker_digest    = "bob"
   preserve_digests = true
   keep_image       = true
   copy_all_images  = true
+  lifecycle {
+    ignore_changes = [source.0.login_password_script, destination.0.login_password_script]
+  }
 }
